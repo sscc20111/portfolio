fb09a413b9760d9f32f87899ab9664679462bf11
sscc20111 <mw92559404@gmail.com>
2023-10-02 18:27:03 +0900

    header 완료(이후작업- intro 함수형으로 변경)

--- src/component/header.js
+++ src/component/header.js
@@ -3,90 +3,61 @@ import { useLocation, Link } from 'react-router-dom';
 
 import LogoApp from '../js/intro_App'
 
-// new LogoApp(this.main,{
-//     size : 150,
-//     background : {
-//         backgroundStyles : 'img',
-//         src : logoImg,
-//         globalAlpha : 1
-//     },
-//     shadow : 'true',
-//     shadowOption : {
-//         color : 'rgba(0, 0, 0, 0.6)',
-//         blur : 15,
-//         x : -5,
-//         y : 5
-//     },
-//     speed : 300
-// });
 
-const inrto = () => {
-    return (
-        <h1>intro</h1>
-    )
+const headerLoad = () => {
+    const logoElement = document.querySelector('.logo_header');
+
+    if(logoElement){
+        new LogoApp('.logo_line',{
+            size : 170,
+            background : {
+                backgroundStyles : 'line',
+            },
+            speed : 300
+        });
+        new LogoApp(".logo_header", {
+            size: 150,
+            background: {
+            backgroundStyles: 'color',
+            backgroundColor: '#80B1C2',
+            globalAlpha: 0.8
+            },
+            speed: 350
+        });
+    }else{
+        setTimeout(() => {
+            headerLoad();
+        }, 100);
+    }
 }
 
-// const testtt = () => {
-//     new LogoApp('.logo_header',{
-//         size : 150,
-//         background : {
-//             backgroundStyles : 'color',
-//             backgroundColor : '#80B1C2',
-//             globalAlpha : 0.8
-//         },
-//         speed : 1000
-//     });
-// }
-const headerOn = () => {
-  
-    return (
-        <h2>
-            <Link className="" to="/about"> <div className='logo_header'></div></Link>
-        </h2>
-    )
-}
 
 const Header = () => {
-    const [test, testset] = useState(inrto);
     const location = useLocation();
-    const [prevLocation, setPrevLocation] = useState(location);
+    const [prevLocation, setPrevLocation] = useState('/');
 
     useEffect(() => {
-    // 이전 경로와 현재 경로가 다르고, IntroPage에서 AboutPage로 이동한 경우에만 'start'로 설정
-    if (prevLocation.pathname === '/' && location.pathname === '/about') {
-        testset(headerOn)
-        // testtt()
-    }
-    if (location.pathname === '/'){
-        testset(inrto);
-        console.log(inrto)
-    }
-
-    // 현재 경로를 이전 경로로 업데이트
-    setPrevLocation(location);
+        if(location.pathname !== '/'){
+            if(prevLocation === '/' || prevLocation.pathname === '/'){
+                headerLoad()
+            }
+        }
+        
+        // 현재 경로를 이전 경로로 업데이트
+        setPrevLocation(location);
     }, [location]);
 
-    useEffect(() => {
-        const logoElement = document.querySelector('.logo_header');
-        console.log(logoElement)
-        setTimeout(() => {
-            if (logoElement) {
-              new LogoApp(".logo_header", {
-                size: 150,
-                background: {
-                  backgroundStyles: 'color',
-                  backgroundColor: '#80B1C2',
-                  globalAlpha: 0.8
-                },
-                speed: 1000
-              });
-            } else {
-                console.log('failde')
-            }
-            
-        }, 1000);
-    }, [test]);
-
-    return <header>{test}</header>;
+    return (
+        <header className='header'>
+            {location.pathname !== '/' ? (
+                <h2 >
+                    <Link className="logo_header position-absolute top-50 start-50 z-1" to="/"></Link>
+                    <div className='logo_line position-absolute top-50 start-50 z-2'></div>
+                </h2>
+                ) : (
+                <></>
+            )}
+        </header>
+    )
 }
 export default Header;
