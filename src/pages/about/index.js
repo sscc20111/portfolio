8e2500db351b787b367f7c171866455c6776c55f
남민우 <sscc20111@naver.com>
2023-11-10 02:34:00 +0900

    about페이지 수정(이후작업-grid 페이지와 연결되는 모션추가)

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -53,8 +53,8 @@ const TestApp = () => {
         gsap.set(main, {xPercent: -50, yPercent: -50, scale:0})
         gsap.set(background, {xPercent: -50, yPercent: -50, scale:0})
     
-        gsap.to(main, {scale: 1, duration:0.5, delay:0.6,  ease: Power1.easeInOut})
-        gsap.to(background, {scale: 1, duration:0.5, delay:0.2,  ease: Power1.easeInOut})
+        gsap.to(main, {scale: 1, duration:0.5, delay:2.1,  ease: Power1.easeInOut})
+        gsap.to(background, {scale: 1, duration:0.5, delay:1.7,  ease: Power1.easeInOut})
     }
     
     useEffect(() => {
@@ -62,36 +62,47 @@ const TestApp = () => {
         introSet('.imgset', '.backgroundset');
     }, []);
     return(
-        <Container className="about transitionBox bg-white" style={{paddingTop:'100px'}}>
-            <Stack gap={5} direction='horizontal' style={{flexWrap: 'wrap'}}>
-                <div className='contentBox position-relative z-0'>
-                    <div className='imgBox'>
-                        <div className='imgset position-absolute top-50 start-50 z-3'></div>
-                        <div className='backgroundset position-absolute top-50 start-50 z-2'></div>
-                        <img src={backgroundImg}></img>
+        <Container className="about transitionBox bg-white pt-5" style={{maxWidth:'1000px'}}>
+            <Stack gap={5}>
+                <Stack className='contentBox justify-content-between align-items-end flex-wrap z-0' direction='horizontal'>
+                    <div className='w-50' style={{maxWidth:'450px'}}>
+                        <div className='imgBox position-relative'>
+                            <div className='imgset position-absolute top-50 start-50 z-3'></div>
+                            <div className='backgroundset position-absolute top-50 start-50 z-2'></div>
+                            <img src={backgroundImg}></img>
+                        </div>
                     </div>
-                    <ul className='textBox'>
-                        <li><span><FontAwesomeIcon icon={faPhone} /></span><p></p></li>
-                        <li><span><FontAwesomeIcon icon={faAt} /></span><p></p></li>
-                        <li><span><FontAwesomeIcon icon={faGithub} /></span><p></p></li>
-                    </ul>
-                </div>
-                <div className='contentBox ms-auto z-1'>
-                    <div className='nameWrap'>
-                        <h3>남민우</h3>
-                        <h3>Nam MinWoo</h3>
+                    <div className='nameWrap w-50 ps-3'>
+                        <h3 style={{fontSize:'4.5rem'}}>남민우</h3>
+                        <h3 style={{fontSize:'2rem'}} className=' fw-normal'>Nam MinWoo</h3>
+                        <ul className='textBox pt-3 ps-0'>
+                            <li className='d-flex fs-5 align-items-center mb-2'>
+                                <FontAwesomeIcon icon={faPhone} className='me-3' />
+                                <p className='mb-0'>010-9255-9404</p>
+                            </li>
+                            <li className='d-flex fs-5 align-items-center mb-2'>
+                                <FontAwesomeIcon icon={faAt} className='me-3' />
+                                <p className='mb-0'>sscc20111@naver.com</p>
+                            </li>
+                            <li className='d-flex fs-5 align-items-center mb-2'>
+                                <FontAwesomeIcon icon={faGithub} className='me-3' />
+                                <a href='https://github.com/sscc20111' target='_blangk'>github</a>
+                            </li>
+                        </ul>
                     </div>
-                    <Stack gap={2} className='introWrap'>
+                </Stack>
+                <div className='mb-4 z-1'>
+                    <Stack gap={2} className='introWrap lh-base'>
                         <p>
                             안녕하세요, 프론트엔드 개발자 남민우입니다.
                         </p>
                         <p>
-                            저는 퍼블리셔로서의 경험을 바탕으로 사용자에게 <span className="bg-warning fw-normal rounded px-2 text-nowrap">최상의 경험을 제공</span>하기 위해 노력하는 개발자입니다. 
+                            저는 퍼블리셔로서의 경험을 바탕으로 사용자에게 <span className="bg-warning fw-normal rounded px-2 text-nowrap">최상의 경험을 제공</span> 하기 위해 노력하는 개발자입니다. <br />
                             프로젝트의 목표와 방향에 따라 최적화된 웹 서비스를 개발하기 위해 동료 개발자들과의 
                             <span className="bg-warning fw-normal rounded px-2 text-nowrap">능동적인 커뮤니케이션</span>이 필수라고 믿습니다.
                         </p>
                         <p>
-                            작업의 효율성과 생산성을 중요하게 생각하며, 끊임없이 진화하는 IT 시장에 발맞춰 
+                            작업의 효율성과 생산성을 중요하게 생각하며, 끊임없이 진화하는 IT 시장에 발맞춰 <br />
                             항상 새로운 지식을 습득하고 싶습니다. 제 노력과 열정을 통해 뛰어난 웹 개발자로서 성장하고 싶습니다.
                         </p>
                         <p>
@@ -99,88 +110,48 @@ const TestApp = () => {
                         </p>
                     </Stack>
                 </div>
-                <div className="contentBox">
-                    <h4 className="mb-3 fw-bold text-warning-emphasis">저는 이런 것을 다룰줄압니다.</h4>
-                    <Stack gap={2}>
+                <div className="">
+                    <h4 className="mb-4 fs-2 fw-bold text-warning-emphasis">WORK EXPERIENCE</h4>
+                    <ul className='ps-0' style={{maxWidth:'400px'}}>
+                        <li className='d-flex align-items-center mb-2' style={{fontSize:'1rem'}}>
+                            <p className="mb-0 w-50">(주)이프론트</p>
+                            <span className='w-50'>2021.11.24 ~ 2022.12.30</span>
+                        </li>
+                        <li className='d-flex align-items-center' style={{fontSize:'1rem'}}>
+                            <p className="mb-0 w-50">IIBI 방송정보국제교육원</p>
+                            <span className='w-50'>2021.05.25 ~ 2021.10.05</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="">
+                    <h4 className="mb-4 fs-2 fw-bold text-warning-emphasis">SKILLS</h4>
+                    <Stack gap={4}>
                         <div>
                             <h5 className="fw-semibold">Frontend</h5>
-                            <ListGroup>
-                                <ListGroupItem className="border-0 py-1">React</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">JQuery</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">Bootstrap</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">Vanilla JS</ListGroupItem>
+                            <ListGroup className='gap-1 ps-3'>
+                                <ListGroupItem className="border-0 p-0">React</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">JQuery</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">Bootstrap</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">GSAP</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">Vanilla JS</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">Figma</ListGroupItem>
                             </ListGroup>
                         </div>
                         <div>
                             <h5 className="fw-semibold">Backend</h5>
-                            <ListGroup>
-                                <ListGroupItem className="border-0 py-1">Express</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">SQL</ListGroupItem>
+                            <ListGroup className='gap-1 ps-3'>
+                                <ListGroupItem className="border-0 p-0">Express</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">SQL</ListGroupItem>
                             </ListGroup>
                         </div>
                         <div>
                             <h5 className="fw-semibold">Version Control</h5>
-                            <ListGroup>
-                                <ListGroupItem className="border-0 py-1">Git Hub</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">SourceTree</ListGroupItem>
-                                <ListGroupItem className="border-0 py-1">Figma</ListGroupItem>
+                            <ListGroup className='gap-1 ps-3'>
+                                <ListGroupItem className="border-0 p-0">Git Hub</ListGroupItem>
+                                <ListGroupItem className="border-0 p-0">SourceTree</ListGroupItem>
                             </ListGroup>
                         </div>
                     </Stack>
-                </div>
-                <div className="contentBox">
-                    <h4 className="mb-3 fw-bold text-warning-emphasis">저는 이런 것을 배웠습니다.</h4>
-                    <Row className="careerWrap">
-                        <Col className="relevant">
-                            <h5 className="text-center">관련 이력</h5>
-                            <ListGroup>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col className="text-end"><span>2021.05.25 - 2021.10.05</span></Col>
-                                        <Col className="text-center"><p className="mb-0">IIBI 방송정보국제교육원</p></Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col className="text-end"><span>2021.11.24 ~ 2022.12.30</span></Col>
-                                        <Col className="text-center"><p className="mb-0">(주)이프론트</p></Col>
-                                    </Row>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Col>
-                        <Col className="additional">
-                            <h5 className="text-center">기타 이력</h5>
-                            <ListGroup>
-                                <ListGroupItem>
-                                    <Row>
-                                        <Col className="text-end"><span>2018.00.00 ~ 2019.00.00</span></Col>
-                                        <Col className="text-center"><p className="mb-0">기아공장</p></Col>
-                                    </Row>
-                                </ListGroupItem>
-                                <ListGroupItem>
-                                    <Row>
-                                        <Col className="text-end"><span>2019.00.00 ~ 2020.00.00</span></Col>
-                                        <Col className="text-center"><p className="mb-0">(주)아이엠티</p></Col>
-                                    </Row>
-                                </ListGroupItem>
-                            </ListGroup>
-                        </Col>
-                    </Row>
-                </div>
-                <div className="contentBox">
-                    <h4 className="mb-3 fw-bold text-warning-emphasis">저는 이런 작업경험이 있습니다.</h4>
-                    <ListGroup>
-                        <ListGroupItem className="border-0 py-1"><a href="http://wowm.1004home.kr/" target="_blank" rel="noopener noreferrer">와우엠</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://iaan.1004home.kr/" target="_blank" rel="noopener noreferrer">이안</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://jeanficial.1004home.kr/" target="_blank" rel="noopener noreferrer">장피셜</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://ccmlaw.1004home.kr/" target="_blank" rel="noopener noreferrer">충만법무법인</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://com2verse.1004home.kr/" target="_blank" rel="noopener noreferrer">컴투버스</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://muirim.com/" target="_blank" rel="noopener noreferrer">무이림</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="https://www.kandesign.kr/" target="_blank" rel="noopener noreferrer">칸디자인</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://pidotech.1004home.kr/" target="_blank" rel="noopener noreferrer">피도텍</a></ListGroupItem>
-                        <ListGroupItem className="border-0 py-1"><a href="http://apsun.1004home.kr/" target="_blank" rel="noopener noreferrer">앞썬</a></ListGroupItem>
-                    </ListGroup>
-                    <Cardslide projects={'http://muirim.com/'}>무이림</Cardslide>
                 </div>
                 <Link to="/projects">
                     project page
