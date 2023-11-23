b28fa294b0bfe4b5af31d8f24854d321b317f821
남민우 <sscc20111@naver.com>
2023-11-24 02:53:14 +0900

    toyProjects-CardGame(기본 동작 구현)

--- src/pages/projects/CardGame/index.js
+++ src/pages/projects/CardGame/index.js
@@ -1,63 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Container, FloatingLabel, FormControl, Stack } from "react-bootstrap"
+import gsap from 'gsap';
 
 import './style.css'
 
 
 const Setting = ({Submit}) => {
-    const [Level, setLevel] = useState('');
+    const [Total, setTotal] = useState('');
 
-    const submit = (Level) => {
-        Submit(Level)
-        setLevel('')
+    const submit = (Total) => {
+        Submit(Total)
+        setTotal('')
     }
 
-
-    return(
-        <>
+    return(<>
         <FloatingLabel className="levelInput mb-2" controlId="" label='카드 개수를 짝수로 입력하세요(최대:20)'>
-            <FormControl type="number" placeholder="Game Level Choice" value={Level} onChange={(e) => setLevel(e.target.value)}></FormControl>
+            <FormControl type="number" placeholder="Game Level Choice" value={Total} onChange={(e) => setTotal(e.target.value)}></FormControl>
         </FloatingLabel>
-        <Button className='' onClick={()=>submit(Level)}>시작!</Button>
-        </>
-
-    )
+        <Button className='' onClick={()=>submit(Total)}>시작!</Button>
+    </>)
 }
 
-const Cards = ({ count }) => {
-    const divs = Array.from({ length: count }, (_, index) => (
-        <div key={index + 1} className='testCard'>{index + 1}</div>
-    ));
+const Cards = ({ Total, Click }) => {
+    const Color = ["red", "orange", "yellow", "green", "white", "black", "cyan", "tomato", "pink", "blue"];
+    const colorSlice = Color.slice(0, Total/2);
+    const ArrayColor = colorSlice.concat(colorSlice)
+    const [CardArray,setCardArray] = useState('')
 
-    return <>{divs}</>;
+    const shuffleArray = (array) => {// 배열 섞기
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];// 배열의 요소를 서로 교환
+        }
+        setCardArray(ArrayColor)
+    }
+
+    const CardClick = (e) => {
+        const target = e.target;
+        if(target.classList.contains('ok') === false){
+            target.classList.add('ok')
+            Click(e)
+        }
+    }
+
+    useEffect(()=> {
+        shuffleArray(ArrayColor);
+    },[])
+
+    return (<>
+        {Array.from({ length: Total }, (_, index) => (
+            <div key={index + 1} data-key={index + 1} className='Card' onClick={(e)=>CardClick(e)}>
+                <div className='cardInner'>
+                    <div className='cardFront' style={{background:`${CardArray[index]}`}}></div>
+                    <div className='cardBack'></div>
+                </div>
+            </div>
+        ))}
+        <button onClick={() => shuffleArray(ArrayColor)}>test</button>
+    </>)
 };
 
 const CardGame = () => {
-    const [Count, setCount] = useState('');
+    const [Total, setTotal] = useState('');
     const [inputVisible, setinputVisible] = useState(true)
+    const [Selected,setSelected] = useState('');
 
-    const start = (Level) => {
+    const start = (total) => {
         setinputVisible(false);
 
-        const count = Level > 20 ? 20 : Level; //20개 초과 제한
-        setCount(count);
+        const totalLimit = total > 20 ? 20 : total; //20개 초과 제한
+        setTotal(totalLimit);
     }
 
-    const Color = ['#004e66', '#a79c8e', '#fd999a', '#6d9d88', '#ff5f2e', '#fcbe32'];
-    const test = ['1', '2', '3', '4', '5', '6'];
+    const GameRule = (e) => {
+        const target = e.target;
+        const targetkey = target.getAttribute('data-key');
+        const targetColor = target.querySelector(".cardFront").style.background
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          // 배열의 요소를 서로 교환
-          [array[i], array[j]] = [array[j], array[i]];
+        switch (true) {
+            case Selected === '': //선택된 카드가 있는지 판별
+                setSelected(target); 
+                break;
+            case Selected.getAttribute('data-key') !== targetkey: //동일 카드 판별
+                if (Selected.querySelector(".cardFront").style.background === targetColor) {//동일 색 판별
+                    console.log('ok');//성공
+                    // target.classList.add('ok')
+                    // Selected.classList.add('ok')
+                    setSelected('');
+                } else {
+                    console.log('fail');//실패
+                    setSelected('');
+                    setTimeout(() => {
+                        target.classList.remove('ok')
+                        Selected.classList.remove('ok')
+                    }, 800);
+                }
+                break;
+            default:
+                break;
         }
-      }
-      
-      // 배열 섞기
-      shuffleArray(test);
-      
-      console.log(test); // 섞인 배열 출력
+    }
+
+
     return(
         <div className="transitionBox">
             <div className="CardGame" style={{width:'100vw', height:'100vh'}}>
@@ -67,7 +111,7 @@ const CardGame = () => {
                     </div>
                 ) : (
                     <Stack className='cardWrap position-absolute top-50 start-50 translate-middle' direction="horizontal" gap={3}>
-                        <Cards count={Count}></Cards>
+                        <Cards Total={Total} Click={GameRule}></Cards>
                     </Stack>
                 )}
             </div>
